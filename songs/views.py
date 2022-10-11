79361b333cc20e5bf95f927f8e14f5e65ea9143a
Jiang <jiangyue0380@gmail.com>
2022-10-12 00:48:13 +1100

    created test GET reuqet

--- songs/views.py
+++ songs/views.py
@@ -1,7 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from songs.serializers import SongSerializer
+from songs.models import Song
 
 
 @api_view(['GET'])
 def songs_list(request):
-    return Response('ok!')
+    # query all the songs from database
+    songs = Song.objects.all()
+    # turn python data into JSON data via serializer
+    serializers = SongSerializer(songs, many=True)
+    return Response(serializers.data)

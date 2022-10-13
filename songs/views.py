6d6704b407bfe9f43d98c6858ab689111b2a23b2
Jiang <jiangyue0380@gmail.com>
2022-10-13 22:32:15 +1100

    updated song_detail function for Updated_Song PUT request and Delete_Song DELETE request

--- songs/views.py
+++ songs/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from songs import serializers
 from songs.serializers import SongSerializer
 from songs.models import Song
 
@@ -11,12 +10,14 @@ from songs.models import Song
 @api_view(['GET', 'POST'])
 def songs_list(request):
     if request.method == 'GET':
+        # Get_All_Songs request
         # query all the songs from database
         songs = Song.objects.all()
         # turn python data into JSON data via serializer
         serializers = SongSerializer(songs, many=True)
-        return Response(serializers.data)
+        return Response(serializers.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
+        # Create_Song request
         # pass the new data into the serializer
         # request.data which can access the data on the incoming request
         serializers = SongSerializer(data=request.data)
@@ -35,21 +36,33 @@ def songs_list(request):
         #     return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def song_detail(request, pk):
-
-    # search the song table and return song which its id/pk equals to pk
-    # if the song exists in the database, return the song's info
-    # if the song doesnt exist in the database, return status code 404
-
-    # # method 1
-    # try:
-    #     song = Song.objects.get(id=pk)
-    #     serializers = SongSerializer(song)
-    #     return Response(serializers.data)
-    # except Song.DoesNotExist:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
-    # # method 2
+    # get_object_or_404 is going to query the Song model/database
+    # if a data is found where id=pk, its value is going to store in song variable
+    # otherwise throw a 404 error
     song = get_object_or_404(Song, id=pk)
-    serializer = SongSerializer(song)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        # Get_Song_By_Id request
+        # search the song table and return song which its id/pk equals to pk
+        # if the song exists in the database, return the song's info
+        # if the song doesnt exist in the database, return status code 404
+
+        # # method 1
+        # try:
+        #     song = Song.objects.get(id=pk)
+        #     serializers = SongSerializer(song)
+        #     return Response(serializers.data)
+        # except Song.DoesNotExist:
+        #     return Response(status=status.HTTP_404_NOT_FOUND)
+        # # method 2
+        serializer = SongSerializer(song)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
